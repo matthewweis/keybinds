2051218b8711379c371230e8e46069a53da82941
matthewweis <matthewweiscaps@gmail.com>
2025-08-12 15:05:03 -0500

    add rl binds

---
+++ rl/rm_eos_overlay.bat
@@ -0,0 +1,4 @@
+@echo on
+move /y "C:\Program Files (x86)\Epic Games\Launcher\Portal\Extras\Overlay\EOSOverlayRenderer-Win32-Shipping.exe" "C:\Program Files (x86)\Epic Games\Launcher\Portal\Extras\Overlay\EOSOverlayRenderer-Win32-Shipping.exe.rename"
+move /y "C:\Program Files (x86)\Epic Games\Launcher\Portal\Extras\Overlay\EOSOverlayRenderer-Win64-Shipping.exe" "C:\Program Files (x86)\Epic Games\Launcher\Portal\Extras\Overlay\EOSOverlayRenderer-Win64-Shipping.exe.rename"
+pause
